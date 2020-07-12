d5dca6f226f5b0d43d37240125f30483836834a1
a26232933 <a094868@gmail.com>
2020-07-12 14:09:23 +0800

    取得所有元素

--- goToElement.js
+++ goToElement.js
@@ -1,14 +1,28 @@
-  
 window.onload = () => {
-    console.log("頁面載入完成~");
 
-    var test = document.getElementById("test");
+    // 取得單一元素
+    // var test = document.getElementById("test");
+    // 取得所有元素
+    var elements = document.getElementsByTagName("*");
 
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
+        
+        // 元素.點擊事件
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target");  // 目標屬性
+            var offset = e.getAttribute("data-gte-offset");  // 位移屬性
+    
+            if (target) {
 
-        console.log(target);
-        console.log(offset);
+                var element = document.getElementById(target);      // 目標元素
+        
+                // 視窗.捲動至({ 上方，行為 })
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior: "smooth"
+                })
+            }
+        }
     }
 }
